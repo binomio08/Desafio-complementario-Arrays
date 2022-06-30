0a1bbaea1dc9cb286a9bc6eaf4ed16f6eecddd75
Leandro Troncoso <binomio08@hotmail.com>
2022-06-29 23:12:18 -0300

    arrays

---
+++ index.js
@@ -0,0 +1,24 @@
+alert("Bienvenid@ a\n**********************\nPAGO MIS CUENTAS\n**********************")
+
+let nombreUsuario = prompt("Ingrese su Usuario")
+
+class Servicios {
+    constructor(servicio, monto){
+        this.servicio = servicio.toUpperCase()
+        this.monto = monto
+    }
+    
+}
+
+function agregarServicios() {
+    let totalServicios = parseInt(prompt("Hola "+nombreUsuario.toUpperCase() + " Ingrese cantidad de Servicios a Pagar"))
+    let servicios = []
+    for (let i = 0; i< totalServicios; i++){
+        let servicio = prompt("Ingrese nombre del servivio a pagar ")
+        let monto = parseInt(prompt("Ingrese el monto que figura en su factura"))
+        let resgistrarServios = new Servicios(servicio, monto)
+        servicios.push(resgistrarServios)
+    }
+    return servicios
+}
+agregarServicios()
