e6ff856fa057610c946adf6da1f9f6d927afe5ac
Leandro Troncoso <binomio08@hotmail.com>
2022-06-30 00:05:02 -0300

    segundo commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let nombreUsuario = prompt("Ingrese su Usuario")
 class Servicios {
     constructor(servicio, monto){
         this.servicio = servicio.toUpperCase()
-        this.monto = monto
+        this.monto = "$" + monto
     }
     
 }
@@ -13,12 +13,26 @@ class Servicios {
 function agregarServicios() {
     let totalServicios = parseInt(prompt("Hola "+nombreUsuario.toUpperCase() + " Ingrese cantidad de Servicios a Pagar"))
     let servicios = []
+
     for (let i = 0; i< totalServicios; i++){
         let servicio = prompt("Ingrese nombre del servivio a pagar ")
         let monto = parseInt(prompt("Ingrese el monto que figura en su factura"))
+
         let resgistrarServios = new Servicios(servicio, monto)
         servicios.push(resgistrarServios)
     }
     return servicios
 }
-agregarServicios()
+function monstrarServicios(servicios){
+    for(const servicio of servicios){
+        console.log(servicio)
+        console.log(servicio.monto)
+    }
+}
+
+function main(){
+    let serviocioIngresados = agregarServicios()
+    monstrarServicios(serviocioIngresados)
+}
+
+main()
